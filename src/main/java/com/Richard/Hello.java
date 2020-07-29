1578168614a3024041865bd0d3ea844541388554
Richard <richeal0718@gmail.com>
2020-07-29 23:28:38 +0800

    Initial commit

---
+++ src/main/java/com/Richard/Hello.java
@@ -0,0 +1,13 @@
+package com.Richard;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//        new Person().hello();
+        Person p = new Person();
+        p.hello();
+    }
+
+}
